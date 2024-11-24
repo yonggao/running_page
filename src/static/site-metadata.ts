344d16606e413d87661da0734a8e00ca988e8d6e
Yonggao Pan <pan.yonggao@qq.com>
2024-11-24 20:36:59 +0800

    sync from personal keep

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,19 +10,11 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
+  siteTitle: '老潘的跑步生涯',
   siteUrl: 'https://yihong.run',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-    },
   ],
 };
 
